e2df1bd60da3b77596f79a851c7fa0b75f55e7d9
Javier E. <jaedns@gmail.com>
2026-05-18 12:09:35 -0300

    Add feat to edit cards (#9)

--- lib/components/new_card_dialog.dart
+++ lib/components/new_card_dialog.dart
@@ -4,9 +4,8 @@ import 'package:go_router/go_router.dart';
 import '../graphql/schema.graphql.dart';
 import '../graphql/mutations/create_card.graphql.dart';
 import '../graphql_client.dart';
-import 'form_container.dart';
+import 'card_form.dart';
 import 'snackbar_alert.dart';
-import 'text_input_field.dart';
 
 Future<dynamic> showNewCardDialog(BuildContext context, {required listId}) {
   return showDialog(
@@ -17,78 +16,45 @@ Future<dynamic> showNewCardDialog(BuildContext context, {required listId}) {
   );
 }
 
-class _NewCardForm extends StatefulWidget {
-  const _NewCardForm({required this.listId});
+class _NewCardForm extends StatelessWidget {
+  _NewCardForm({required this.listId});
 
   final String listId;
 
-  @override
-  _NewCardFormState createState() => _NewCardFormState();
-}
-
-class _NewCardFormState extends State<_NewCardForm> {
   final _formNewCard = GlobalKey<FormState>();
 
-  String _content = '';
-  String? _errorContent;
-
-  void _attemptToCreateCard() async {
-    setState(() {
-      _errorContent = null;
-    });
-
-    if (_formNewCard.currentState?.validate() == true) {
-      _formNewCard.currentState?.save();
-      final graphQLClient = context.graphQLClient.value;
-      final result = await graphQLClient.mutate$CreateCard(
-        Options$Mutation$CreateCard(
-          variables: Variables$Mutation$CreateCard(
-            params: Input$CardParams(listId: widget.listId, content: _content),
-          ),
+  Future<Map<String, dynamic>?> _attemptToCreateCard(BuildContext context, String content) async {
+    final graphQLClient = context.graphQLClient.value;
+    final result = await graphQLClient.mutate$CreateCard(
+      Options$Mutation$CreateCard(
+        variables: Variables$Mutation$CreateCard(
+          params: Input$CardParams(listId: listId, content: content),
         ),
-      );
+      ),
+    );
 
-      if (!mounted) {
-        return;
-      }
+    if (!context.mounted) {
+      return null;
+    }
 
-      final errors = result.exception?.graphqlErrors.first;
+    final errors = result.exception?.graphqlErrors.first;
+    final createdCard = result.parsedData?.createCard;
 
-      if (result.parsedData?.createCard != null) {
-        showSnackBarAlert(context, 'Card created successfully');
+    if (createdCard != null) {
+      showSnackBarAlert(context, 'Card created successfully');
 
-        context.pop();
-      } else {
-        showSnackBarAlert(context, errors?.message ?? 'Failed to create card');
+      context.pop();
 
-        setState(() {
-          _errorContent = errors?.extensions?['params']['content']?['message'];
-        });
-      }
+      return null;
     } else {
-      showSnackBarAlert(context, 'Failed to create card');
+      showSnackBarAlert(context, errors?.message ?? 'Failed to create card');
+
+      return errors?.extensions?['params'];
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return FormContainer(
-      formKey: _formNewCard,
-      width: 480,
-      padding: EdgeInsets.all(16),
-      onSubmit: _attemptToCreateCard,
-      fields: [
-        Text('New card', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-        TextInputField(
-          labelText: 'Content',
-          errorText: _errorContent,
-          required: true,
-          maxLines: 4,
-          onSaved: (value) {
-            _content = value ?? '';
-          },
-        ),
-      ],
-    );
+    return CardForm(formKey: _formNewCard, onSubmit: (content) => _attemptToCreateCard(context, content));
   }
 }
